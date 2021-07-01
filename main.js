a937181f37189c16d2ede0f41e7e4ed06a558abe
benberryallwood <b.berry-allwood@protonmail.com>
2021-07-01 09:31:55 +0100

    Correct indentation

--- main.js
+++ main.js
@@ -1,8 +1,8 @@
 var quotes = [
-      {quote: "The best programs are the ones written when the programmer is supposed to be working on something else.", author: "Melinda Varian"},
-      {quote: "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.", author: "Marilyn Monroe"},
-      {quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.", author: "Albert Einstein"},
-      {quote: "So many books, so little time.", author: "Frank Zappa"}
+    {quote: "The best programs are the ones written when the programmer is supposed to be working on something else.", author: "Melinda Varian"},
+    {quote: "I'm selfish, impatient and a little insecure. I make mistakes, I am out of control and at times hard to handle. But if you can't handle me at my worst, then you sure as hell don't deserve me at my best.", author: "Marilyn Monroe"},
+    {quote: "Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.", author: "Albert Einstein"},
+    {quote: "So many books, so little time.", author: "Frank Zappa"}
 ]
 
 var quoteNum;
