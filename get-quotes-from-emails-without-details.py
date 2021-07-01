a094e9058026b07209013f5f5131aa3d82161c27
benberryallwood <b.berry-allwood@protonmail.com>
2021-07-01 14:52:16 +0100

    Get email fetching working

--- get-quotes-from-emails-without-details.py
+++ get-quotes-from-emails-without-details.py
@@ -1,8 +1,22 @@
 #!/bin/env python3
-import pandas as pd
-import datetime, os, glob
+#import os
 import email, imaplib
 
-cwd = os.getcwd()
+#cwd = os.getcwd()
 EMAIL_UN  = ''
 EMAIL_PW = ''
+
+with imaplib.IMAP4_SSL('imap.gmail.com', 993) as M:
+    M.login(EMAIL_UN, EMAIL_PW)
+    M.select()
+    typ, data = M.search(None, 'BODY "Quote of the Week"')
+    for num in data[0].split():
+        typ, data = M.fetch(num, '(RFC822)')
+        email_body = data[0][1]
+        email_text = str(email_body)
+        # print('Message %s\n%s\n' % (num, data[0][1]))
+        line_list = email_text.splitlines()
+        for line in line_list:
+            if "Quote of the Week" not in line:
+                line_list.remove(line)
+        #print(line_list)
