43d0d0350bf0ac4a34ce72b18afd8f3b9957359d
benberryallwood <b.berry-allwood@protonmail.com>
2021-07-07 19:50:05 +0100

    Try to get json working

--- get-quotes-from-emails-without-details.py
+++ get-quotes-from-emails-without-details.py
@@ -6,17 +6,20 @@ import re
 EMAIL_UN = ''
 EMAIL_PW = ''
 
-quotes = []
+quotes = dict()
 
 with imaplib.IMAP4_SSL('imap.gmail.com', 993) as M:
     M.login(EMAIL_UN, EMAIL_PW)
     M.select()
     typ, data = M.search(None, 'BODY "Quote of the Week"')
+    i = 0
     for mail_id in data[0].split():
         typ, data = M.fetch(mail_id, '(RFC822)')
         email_body = data[0][1]
         email_text = str(email_body)
         mail = email.message_from_string(email_text)
+        # print('Message %s\n%s\n' % (num, data[0][1]))
+        # print(mail)
         line_list = email_text.split('\\r\\n\\r\\n')
         for line in line_list:
             if "Quote of the Week" in line:
@@ -32,7 +35,10 @@ with imaplib.IMAP4_SSL('imap.gmail.com', 993) as M:
 
                 q = re.search('"(.*)" - ', line).group(1)  # quote
                 a = re.search('" - (.*)', line).group(1)  # author
-                quotes.append({'quote': q, 'author': a})
+                quotes[i] = {'quote': q, 'author': a}
+                i += 1
 
-with open("quotes.txt", "w") as Q:
+# print(quotes)
+
+with open("quotes.json", "w") as Q:
     Q.write(str(quotes))
